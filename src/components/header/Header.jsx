e088a20424f225035997861bec9a5764bbb41978
hirodinn <hirebikila1@gmail.com>
2025-10-18 23:22:57 +0300

    fix styling issues

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ export function Header({ width, height }) {
   return (
     <header id="home" className={height >= 120 ? "solid" : ""}>
       <img src="header-icons/edusity.png" />
-      {width >= 775 ? <Navigators orientation="horizontal" /> : <Sidebar />}
+      {width >= 790 ? <Navigators orientation="horizontal" /> : <Sidebar />}
     </header>
   );
 }
