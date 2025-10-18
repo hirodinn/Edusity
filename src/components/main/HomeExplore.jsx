8af2359fd4940ceb95d641b51e0995c3d062ee62
hirodinn <hirebikila1@gmail.com>
2025-10-18 12:26:04 +0300

    Export the first section to its own file

---
+++ src/components/main/HomeExplore.jsx
@@ -0,0 +1,19 @@
+import "./HomeExplore.css";
+export function HomeExplore() {
+  return (
+    <section className="home-explore">
+      <div className="information">
+        <h1>We Ensure better education for a better world</h1>
+        <p>
+          Our cutting-edge curriculum is designed to empower students with the
+          knowledge, skills, and experiences needed to excel in the dynamic
+          field of education
+        </p>
+        <button className="arrow">
+          <p>Explore more</p>
+          <img src="home/arrow.png" />
+        </button>
+      </div>
+    </section>
+  );
+}
